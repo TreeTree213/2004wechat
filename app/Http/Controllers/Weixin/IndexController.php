d1545c89fd0532d73539241fb8658282e7893f52
ziheng <2402994714@qq.com>
2020-11-06 16:52:33 +0800

    微信接入

--- app/Http/Controllers/Weixin/IndexController.php
+++ app/Http/Controllers/Weixin/IndexController.php
@@ -7,32 +7,38 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 class IndexController extends Controller
 {
-    public function index(){
-    	$result = $this->checkSignature();
-    	if($result){
-    		echo $_GET["echostr"];
-    		exit;
-    	}
-    }
+   //获取access_token
+ public function getAccessToken(){
 
-    private function checkSignature(){
+    $key = 'WX:access_token';
 
-    $signature = $_GET["signature"];
-    $timestamp = $_GET["timestamp"];
-    $nonce = $_GET["nonce"];
-	
-    $token = config('weixin.Token');
-    $tmpArr = array($token, $timestamp, $nonce);
-    sort($tmpArr, SORT_STRING);
-    $tmpStr = implode( $tmpArr );
-    $tmpStr = sha1( $tmpStr );
-    
-    if( $tmpStr == $signature ){
-        return true;
+    //检查是否有token
+    $token = Redis::get($key);
+    if($token){
+        echo "有缓存";echo'</br>';
+        
     }else{
-        return false;
+        echo "无缓存";
+    $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".env('WX_APPID')."&secret=".env('WX_APPSECRET');
+
+    $response = file_get_contents($url);
+
+ 
+
+    $data = json_decode($response,true);
+    $token = $data['access_token'];
+
+
+    //保存到redis 中  时间为3600
+
+    Redis::set($key,$token);
+    Redis::expire($key,3600);
+
     }
-}
+    
+    echo "access_token: ".$token;
+ }
+
 
 
 }
