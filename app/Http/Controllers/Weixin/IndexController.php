e793826e0ae9846329dfcb07f835f036d59a7911
ziheng <2402994714@qq.com>
2020-11-06 16:19:04 +0800

    微信接入

--- app/Http/Controllers/Weixin/IndexController.php
+++ app/Http/Controllers/Weixin/IndexController.php
@@ -34,36 +34,6 @@ class IndexController extends Controller
     }
 }
 
-//获取access_token
- public function getAccessToken(){
 
- 	$key = 'WX:access_token';
-
- 	//检查是否有token
- 	$token = Redis::get($key);
- 	if($token){
- 		echo "有缓存";echo'</br>';
- 		
- 	}else{
- 		echo "无缓存";
- 	$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".env('WX_APPID')."&secret=".env('WX_APPSECRET');
-
- 	$response = file_get_contents($url);
-
- 
-
- 	$data = json_decode($response,true);
- 	$token = $data['access_token'];
-
-
- 	//保存到redis 中  时间为3600
-
- 	Redis::set($key,$token);
- 	Redis::expire($key,3600);
-
- 	}
- 	
- 	echo "access_token: ".$token;
- }
 
 }
